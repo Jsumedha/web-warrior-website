7acd0d1a24cf7344b1aa1eef084075ff88f10f06
Jsumedha <jsumedha99@gmail.com>
2021-04-07 20:10:20 +0530

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default function about() {
+    return (
+        <Layout>
+        <div>
+            <h1>About page</h1>
+            <p>Lorem ipsum</p>
+            <p>Lorem ipsum</p>
+            <p>Lorem ipsum </p>
+        </div>
+        </Layout>
+    )
+}
+
